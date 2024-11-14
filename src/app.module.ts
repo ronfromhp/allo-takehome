bd9fc04e7b4dd6d72afa5c517cc41072aded26a1
ronfromhp <ronaldsamuel2002@gmail.com>
2024-11-14 16:52:11 +0530

    add project setup instructions

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DoctorModule } from './doctor/doctor.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, DoctorModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule, 
+    DoctorModule, 
+    PrismaModule,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
